8694c5c2d8dd33641c88d53a44da85703616b1f4
A-V-Kuzmich <a.v.kuzmi4@gmail.com>
2022-05-12 20:54:36 +0300

    fix redux

--- src/index.js
+++ src/index.js
@@ -2,23 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './components';
-import { store, persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import './index.css';
-import './stylesheet/base.css';
 import Global from './globalStyles';
+import './index.css';
+// import './stylesheet/base.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-         <Global />
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <BrowserRouter>
+      <Global />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
