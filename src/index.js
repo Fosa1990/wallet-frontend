a8dcb62a0640d5084ef88986b42608c80fbb53e7
A-V-Kuzmich <a.v.kuzmi4@gmail.com>
2022-05-12 21:04:58 +0300

    fixt index

--- src/index.js
+++ src/index.js
@@ -2,24 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './components';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
 import '../node_modules/modern-normalize/modern-normalize.css';
 
 import Global from './globalStyles';
-import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <Global />
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <BrowserRouter>
+      <Global />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
