dca56b71b88b423e7c3f620776c21cd8fb09f6d7
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-05-13 11:56:26 +0300

    add base styles for currency

--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,40 +1,97 @@
 import styled from 'styled-components';
 import { size } from '../../stylesheet/utils/stylesVars';
+import {
+  iconBgValueCl,
+  iconBgCl,
+  accentTextCl,
+  poppinsFont,
+  circleFont,
+} from '../../stylesheet/utils/stylesVars';
 
 const Table = styled.table`
-${size.mobile} {
-    width: 320px;
-    }
-   ${size.tablet} {
-    width: 768px;
-     }
+  background-color: ${iconBgValueCl};
+  border-radius: 30px 30px 30px 30px;
+  border-collapse: collapse;
+  ${size.mobile} {
+    width: 280px;
+    height: 174px;
+  }
+  ${size.tablet} {
+    width: 336px;
+    height: 182px;
+  }
   ${size.desktop} {
-    width: 1280px;
-    }
-`
+    width: 393px;
+    height: 347px;
+  }
+`;
+
+const Thead = styled.thead`
+  background-color: ${iconBgCl};
+
+  ${size.mobile} {
+    height: 50px;
+  }
+  ${size.tablet} {
+    height: 50px;
+  }
+  ${size.desktop} {
+    height: 60px;
+  }
+`;
+
+const Tr = styled.tr``;
+
+const Th = styled.th`
+  font: ${poppinsFont};
+  font-size: 700;
+  font-weight: 18px;
+  line-height: 26px;
+  color: white;
+
+  :first-child {
+    border-radius: 30px 0 0 0;
+  }
+  :last-child {
+    border-radius: 0 30px 0 0;
+  }
+`;
+// почему не работает цвет текста из переменной
+
+const Td = styled.td`
+  font: circleFont;
+  font-size: 400;
+  font-weight: 16px;
+  line-height: 23px;
+  color: white;
+  padding: 0 20px;
+
+  :not(:first-child) {
+    text-align: center;
+  }
+`;
 
 export default function Currency() {
   return (
     <div>
-      Component: Currency
       <Table>
-        <thead>
-          <tr>
-            <th>Currency</th>
-            <th>Buy</th>
-            <th>Sell</th>
-          </tr>
-        </thead>
+        <Thead>
+          <Tr>
+            <Th>Currency</Th>
+            <Th>Buy</Th>
+            <Th>Sell</Th>
+          </Tr>
+        </Thead>
         <tbody>
           <tr>
-            <td>USD</td>
-            <td>33.50</td>
-            <td>34.50</td>
+            <Td>USD</Td>
+            <Td>33.50</Td>
+            <Td>34.50</Td>
           </tr>
           <tr>
-            <td>EUR</td>
-             <td>37.50</td>
-            <td>38.50</td>
+            <Td>EUR</Td>
+            <Td>37.50</Td>
+            <Td>38.50</Td>
           </tr>
         </tbody>
       </Table>
