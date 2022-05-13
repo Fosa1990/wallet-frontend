40c7c30f7121c01c8220321a22092a5d5c2344e3
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-05-13 11:11:43 +0300

    add currency component (initial)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,11 +3,14 @@ import Loader from '../Loader';
 
 import React from 'react';
 
+import Currency from '../Currency/Currency';
+
 export default function App() {
   return (
     <>
       <Loader />
       <h1>Hi</h1>
+      <Currency/>
     </>
   );
 }
--- src/components/Currency/Currency.jsx
+++ src/components/Currency/Currency.jsx
@@ -1,5 +1,43 @@
-export default function Currency () {
+import styled from 'styled-components';
+import { size } from '../../stylesheet/utils/stylesVars';
+
+const Table = styled.table`
+${size.mobile} {
+    width: 320px;
+    }
+   ${size.tablet} {
+    width: 768px;
+     }
+  ${size.desktop} {
+    width: 1280px;
+    }
+`
+
+export default function Currency() {
   return (
-    <div >Component: Currency</div>
-  )
+    <div>
+      Component: Currency
+      <Table>
+        <thead>
+          <tr>
+            <th>Currency</th>
+            <th>Buy</th>
+            <th>Sell</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>USD</td>
+            <td>33.50</td>
+            <td>34.50</td>
+          </tr>
+          <tr>
+            <td>EUR</td>
+             <td>37.50</td>
+            <td>38.50</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 }
