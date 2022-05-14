f605fa32646f9a64e9f8179fcd3c5803b572e445
YuliaTsveleva <tsveleva.yulia@gmail.com>
2022-05-14 15:57:35 +0300

    deleted currency component from app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,11 @@ import Loader from '../Loader';
 
 import React from 'react';
 
-import Currency from '../Currency/Currency';
-
 export default function App() {
   return (
     <>
       <Loader />
       <h1>Hi</h1>
-      <Currency/>
     </>
   );
 }
