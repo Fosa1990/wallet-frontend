af5dc2a09c82dd21aee302e0e3b9045f3083ad93
MariaShebeko <marygro89@gmail.com>
2022-05-14 06:52:37 +0200

    Added Chart and Table for Diagram Tab. (with fake data)

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,5 +1,16 @@
-export default function DiagramTab () {
+import Table from '../Table/Table';
+import Chart from '../Chart/Chart';
+import transactions from '../Table/transactions.json'
+
+
+export default function DiagramTab() {
+  const categories = transactions.map(transaction => transaction.category)
+const colors = transactions.map(transaction => transaction.color);
+const sums = transactions.map(transaction=>transaction.sum)
   return (
-    <div >Component: DiagramTab</div>
+    <>
+      <Chart categories={categories} colors={colors} sums={ sums}/>
+      <Table transactions={transactions} />
+    </>
   )
 }
