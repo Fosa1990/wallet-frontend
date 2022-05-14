1778fed645919ebeb28e05fd2dc2639c37e05568
A-V-Kuzmich <a.v.kuzmi4@gmail.com>
2022-05-14 23:26:35 +0300

    add button

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,12 @@ import './App.css';
 import Loader from '../Loader';
 
 import React from 'react';
+import ButtonAddTransactions from '../ButtonAddTransactions';
 
 export default function App() {
   return (
     <>
+      <ButtonAddTransactions />
       <Loader />
       <h1>Hi</h1>
     </>
--- src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
+++ src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
@@ -1,5 +1,42 @@
-export default function ButtonAddTransactions () {
+import {
+  accentPositiveCl,
+  size,
+  accentNegativeCl,
+} from '../../stylesheet/utils/stylesVars';
+import styled from 'styled-components';
+import sprite from '../../images/svg/sprite.svg';
+
+export default function ButtonAddTransactions() {
+  const Button = styled.button`
+    position: fixed;
+    width: 44px;
+    height: 44px;
+    right: 20px;
+    bottom: 20px;
+    border-radius: 22px;
+    border: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    box-shadow: 0px 6px 15px rgba(36, 204, 167, 0.5);
+    background: ${accentPositiveCl};
+
+    &:hover {
+      background: ${accentNegativeCl};
+    }
+
+    ${size.tablet} {
+      right: 40px;
+      bottom: 40px;
+    }
+  `;
+
   return (
-    <div >Component: ButtonAddTransactions</div>
-  )
+    <Button>
+      <svg width={20} height={20}>
+        <use href={`${sprite}#icon-add`} />
+      </svg>
+    </Button>
+  );
 }
