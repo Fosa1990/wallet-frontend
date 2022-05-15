ba81fa3c1d02f77d10526619ee0ddc92c96d95bd
A-V-Kuzmich <a.v.kuzmi4@gmail.com>
2022-05-15 10:07:09 +0300

    add html string

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,10 +3,12 @@ import Loader from '../Loader';
 
 import React from 'react';
 import ButtonAddTransactions from '../ButtonAddTransactions';
+import ModalAddTransactions from '../ModalAddTransactions';
 
 export default function App() {
   return (
     <>
+      <ModalAddTransactions />
       <ButtonAddTransactions />
       <Loader />
       <h1>Hi</h1>
--- src/components/ModalAddTransactions/ModalAddTransactions.jsx
+++ src/components/ModalAddTransactions/ModalAddTransactions.jsx
@@ -1,5 +1,24 @@
-export default function ModalAddTransactions () {
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function ModalAddTransactions() {
+  const notify = () =>
+    toast.error('Error, somesing go wrong', { autoClose: 3000 });
   return (
-    <div >Component: ModalAddTransactions</div>
-  )
+    <div>
+      <h2>Добавить транзакцию</h2>
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer />
+      <form>
+        <input type="checkbox" id="switch" />
+        <label for="switch">Toggle</label>
+        <label>
+          <input type="text" />
+        </label>
+        <label>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </label>
+      </form>
+    </div>
+  );
 }
