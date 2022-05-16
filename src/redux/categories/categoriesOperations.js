6be607f4bc82ee8f4f1408822354e7f17f0f9e99
MariaShebeko <marygro89@gmail.com>
2022-05-16 17:46:30 +0200

    added operations for getting categories

---
+++ src/redux/categories/categoriesOperations.js
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const BASE_URL = 'https://amazing-wallet.herokuapp.com/';
+
+axios.defaults.baseURL = BASE_URL;
+
+export const getCategories = createAsyncThunk(
+  'categories/getCategories',
+  async (_, { rejectWithValue }) => {
+    try {
+      const categories = await axios.get('/api/categories?year=2022&month=05');
+      console.log('__getCategories categories', categories);
+
+      return categories.data;
+    } catch (error) {
+      console.log('__getCategories error.message', error.message);
+      return rejectWithValue(error.message);
+    }
+  },
+);
