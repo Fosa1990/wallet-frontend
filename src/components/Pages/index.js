642d4c7270b91c0df389b73ba3922bc09e62aaed
Fosa1990 <fosa1990@gmail.com>
2022-05-17 22:16:40 +0300

    Update index.js

--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,4 +1,4 @@
-export { default as DashBoard } from './DashboardPage';
+export { default as DashBoard } from './DashBoardPage';
 export { default as Login } from './LoginPage';
 export { default as Registration } from './RegistrationPage';
 export { default as Home } from './HomePage';
