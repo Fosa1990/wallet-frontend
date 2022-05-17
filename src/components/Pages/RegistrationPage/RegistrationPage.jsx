0418d523b63047c3d57d59c302c7ee4b3cd1d825
Fosa1990 <fosa1990@gmail.com>
2022-05-17 04:41:30 +0300

    Update RegistrationPage.jsx

--- src/components/Pages/RegistrationPage/RegistrationPage.jsx
+++ src/components/Pages/RegistrationPage/RegistrationPage.jsx
@@ -8,6 +8,16 @@ export default function RegistrationPage() {
   console.log('==REGIN==');
   return (
     <>
+      <>
+        <h1>RegistrationPage</h1>
+        <a href="/">Home page - opened</a>
+        <br />
+        <a href="/login">Login - publicRestricted</a>
+        <br />
+        <a href="/registration">Registration - publicRestricted</a>
+        <br />
+        <a href="/dashboard">Dashboard - private</a>
+      </>
       <ContainerAuth
         imgTab={imgRegisterTab}
         imgTabBest={imgRegisterTabBest}
