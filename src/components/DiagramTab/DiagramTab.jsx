53d6d3822e0d866fa478b39320249a5a7e2266f8
MariaShebeko <marygro89@gmail.com>
2022-05-17 11:45:09 +0200

    added redux to get categories(but it does'nt work maybe due to errors in login form)

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -2,7 +2,38 @@ import Table from '../Table';
 import Chart from '../Chart';
 import Select from '../Select';
 import styled from 'styled-components';
+import { useEffect } from 'react';
+import { useDispatch, useSelector, useStore } from 'react-redux';
+import {getCategories} from '../../redux/categories/categoriesOperations'
 import transactions from '../Table/transactions.json';
+
+// баланс в диаграмму подставить когда будет готов компонент баланс
+export default function DiagramTab() {
+  const dispatch = useDispatch();
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // console.log('searchParams', searchParams);
+  // Данные для рендера диаграмы из json
+  const categories = transactions.map(transaction => transaction.category);
+  const colors = transactions.map(transaction => transaction.color);
+  const sums = transactions.map(transaction => transaction.sum);
+
+  useEffect(() => {
+    dispatch(getCategories)
+  }, [dispatch])
+  
+  return (
+    <>
+      <ChartWrapper>
+        <Balance>
+          &#8372;&nbsp; {transactions.length > 0 ? 'balance' : 0}{' '}
+        </Balance>
+        <Chart categories={categories} colors={colors} sums={sums} />
+      </ChartWrapper>
+      <Select />
+      <Table transactions={transactions} />
+    </>
+  );
+}
 
 const ChartWrapper = styled.div`
   position: relative;
@@ -17,21 +48,3 @@ const Balance = styled.span`
   font-size: 18px;
   line-height: 1.5;
 `;
-// баланс в диаграмму подставить когда будет готов компонент баланс
-export default function DiagramTab() {
-  const categories = transactions.map(transaction => transaction.category);
-  const colors = transactions.map(transaction => transaction.color);
-  const sums = transactions.map(transaction => transaction.sum);
-  return (
-    <>
-      <ChartWrapper>
-        <Balance>
-          &#8372;&nbsp; {transactions.length > 0 ? 'balance' : 0}{' '}
-        </Balance>
-        <Chart categories={categories} colors={colors} sums={sums} />
-      </ChartWrapper>
-      <Select />
-      <Table transactions={transactions} />
-    </>
-  );
-}
