c146f2b939bd6c444e1f2a975ea29094b3c80b5a
Fosa1990 <fosa1990@gmail.com>
2022-05-17 04:41:36 +0300

    Update PublicRoute.jsx

--- src/components/Router/PublicRoute/PublicRoute.jsx
+++ src/components/Router/PublicRoute/PublicRoute.jsx
@@ -3,30 +3,13 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { getIsLoggedIn } from '../../../redux/auth/authSelectors';
 
-// export default function PublicRoute({
-//   children,
-//   restricted = false,
-//   redirectTo = '/',
-// }) {
-//   const isLoggedIn = useSelector(getIsLoggedIn);
-//   console.log('isLoggedIn', isLoggedIn);
-//   const shouldRedirect = isLoggedIn && restricted;
-
-//   return <>{!shouldRedirect ? <Navigate to={redirectTo} /> : children}</>;
-// }
-
 export default function PublicRoute({ children, restricted, redirectTo }) {
-  console.log('__1__');
   const isLoggedIn = useSelector(getIsLoggedIn);
   const shouldRedirect = isLoggedIn && restricted;
 
-  console.log('__isLoggedIn', isLoggedIn);
-  console.log('__restricted', restricted);
-  console.log('__shouldRedirect', shouldRedirect);
+  console.log('==PublicRoute==');
 
-  console.log('__2__');
-
-  return <>{!shouldRedirect ? children : <Navigate to="/home" />}</>;
+  return <>{shouldRedirect ? <Navigate to={redirectTo} /> : children}</>;
 }
 
 PublicRoute.defaultProps = {
