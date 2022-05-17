8e469b8c70dbbe98482297b4776d1f8946862d3f
Fosa1990 <fosa1990@gmail.com>
2022-05-17 04:41:25 +0300

    Update HomePage.jsx

--- src/components/Pages/HomePage/HomePage.jsx
+++ src/components/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,15 @@
 export default function Home() {
+  console.log('==HOME==');
   return (
     <>
-      <h1>HomePage</h1>
-      <a href="/">Home page</a>
+      <h1>RegistrationPage</h1>
+      <a href="/">Home page - opened</a>
       <br />
-      <a href="/login">Login</a>
+      <a href="/login">Login - publicRestricted</a>
       <br />
-      <a href="/registration">Registration</a>
+      <a href="/registration">Registration - publicRestricted</a>
+      <br />
+      <a href="/dashboard">Dashboard - private</a>
     </>
   );
 }
