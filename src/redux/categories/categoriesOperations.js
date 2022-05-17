13e745b81baf9b6fd997f4bfa2e753e328dd008c
MariaShebeko <marygro89@gmail.com>
2022-05-17 11:41:48 +0200

    remove extra slash from base url

--- src/redux/categories/categoriesOperations.js
+++ src/redux/categories/categoriesOperations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const BASE_URL = 'https://amazing-wallet.herokuapp.com/';
+const BASE_URL = 'https://amazing-wallet.herokuapp.com';
 
 axios.defaults.baseURL = BASE_URL;
 
