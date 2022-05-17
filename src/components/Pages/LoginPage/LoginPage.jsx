778c0c7b8769e499948d1dd7f928de7f609445aa
Fosa1990 <fosa1990@gmail.com>
2022-05-17 04:41:28 +0300

    Update LoginPage.jsx

--- src/components/Pages/LoginPage/LoginPage.jsx
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -9,6 +9,16 @@ export default function LoginPage() {
   console.log('==LOGIN==');
   return (
     <>
+      <>
+        <h1>RegistrationPage</h1>
+        <a href="/">Home page - opened</a>
+        <br />
+        <a href="/login">Login - publicRestricted</a>
+        <br />
+        <a href="/registration">Registration - publicRestricted</a>
+        <br />
+        <a href="/dashboard">Dashboard - private</a>
+      </>
       <ContainerAuth
         imgTab={imgLoginTab}
         imgTabBest={imgLoginTabBest}
