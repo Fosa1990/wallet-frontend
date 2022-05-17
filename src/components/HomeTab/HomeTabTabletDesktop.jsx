0d10c1845267c3d99b670cfcdbc01b8c361afc6d
Anatoliy Sulypa <82157713+Fosa1990@users.noreply.github.com>
2022-05-17 20:36:42 +0300

    Update HomeTabTabletDesktop.jsx

--- src/components/HomeTab/HomeTabTabletDesktop.jsx
+++ src/components/HomeTab/HomeTabTabletDesktop.jsx
@@ -2,58 +2,6 @@ import styled from 'styled-components';
 import { size } from '../../stylesheet/utils/stylesVars';
 import { circleFont, accentBgCl } from '../../stylesheet/utils/stylesVars';
 
-const Table = styled.table`
-  border-collapse: collapse;
-  ${size.tablet} {
-    width: 704px;
-    height: 312px;
-  }
-  ${size.desktop} {
-    width: 715px;
-    height: 312px;
-  }
-`;
-
-const Thead = styled.thead`
-  background-color: ${accentBgCl};
-`;
-
-const Tr = styled.tr`
-    border-bottom: 1px solid #dcdcdf;
-    box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.6);
- `;
-
-const Th = styled.th`
-  font: ${circleFont};
-  font-size: 18px;
-  font-weight: 700;
-
-  ${size.tablet} {
-    padding: 16px 0 15px;
-    text-align: center;
-    :first-child {
-      border-radius: 30px 0 0 30px;
-    }
-
-    :last-child {
-      border-radius: 0 30px 30px 0;
-    }
-  }
-`;
-
-const Tbody = styled.tbody``;
-
-const Td = styled.td`
-  font: ${circleFont};
-  font-size: 16px;
-  font-weight: 400;
-  padding: 14px 0;
-
-  ${size.tablet} {
-    text-align: center;
-  }
-`;
-
 export default function HomeTabTabletDesktop() {
   return (
       <Table>
@@ -112,3 +60,56 @@ export default function HomeTabTabletDesktop() {
             </Table>
   );
 }
+
+const Table = styled.table`
+  border-collapse: collapse;
+  ${size.tablet} {
+    width: 704px;
+    height: 312px;
+  }
+  ${size.desktop} {
+    width: 715px;
+    height: 312px;
+  }
+`;
+
+const Thead = styled.thead`
+  background-color: ${accentBgCl};
+`;
+
+const Tr = styled.tr`
+    border-bottom: 1px solid #dcdcdf;
+    box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.6);
+ `;
+
+const Th = styled.th`
+  font: ${circleFont};
+  font-size: 18px;
+  font-weight: 700;
+
+  ${size.tablet} {
+    padding: 16px 0 15px;
+    text-align: center;
+    :first-child {
+      border-radius: 30px 0 0 30px;
+    }
+
+    :last-child {
+      border-radius: 0 30px 30px 0;
+    }
+  }
+`;
+
+const Tbody = styled.tbody``;
+
+const Td = styled.td`
+  font: ${circleFont};
+  font-size: 16px;
+  font-weight: 400;
+  padding: 14px 0;
+
+  ${size.tablet} {
+    text-align: center;
+  }
+`;
+
