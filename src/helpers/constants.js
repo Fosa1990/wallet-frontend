b7d09e0e40aa70fe357b5b462eae26ba9d3fa0c7
MariaShebeko <marygro89@gmail.com>
2022-05-18 16:28:04 +0200

    added constants with colors with categories to create colorful diagram and table

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,1 +1,14 @@
 export const BASE_URL = 'http://localhost:3000/';
+
+export const colors = [
+  { category: 'basic spend', color: '#FED057' },
+  { category: 'products', color: '#FFD8D0' },
+  { category: 'car', color: '#FD9498' },
+  { category: 'household products', color: '#C5BAFF' },
+  { category: 'self care', color: '#6E78E8' },
+  { category: 'child care', color: '#4A56E2' },
+  { category: 'education', color: '#81E1FF' },
+  { category: 'leisure', color: '#24CCA7' },
+  { category: 'other spend', color: '#00AD84' },
+  { category: 'income', color: '#3B74B4' },
+];
