70007ddd7b5d3402e40c543a8493fc48a4742f0c
MariaShebeko <marygro89@gmail.com>
2022-05-19 11:47:30 +0200

    added operation to get categories without query

--- src/redux/categories/categoriesOperations.js
+++ src/redux/categories/categoriesOperations.js
@@ -1,24 +1,21 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// const { getState } = useStore();
-// const { auth } = getState();
-// const token = auth.token;
-// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-
-const BASE_URL = 'https://amazing-wallet.herokuapp.com';
-axios.default.baseURL = BASE_URL;
+// const BASE_URL = 'https://amazing-wallet.herokuapp.com';
+// axios.baseURL = BASE_URL;
 
 export const getCategories = createAsyncThunk(
   'categories/getCategories',
   async (_, { rejectWithValue }) => {
     try {
-      console.log('BEFORe CONST');
-
-      const response = await axios.get('/api/categories?year=2022&month=05');
-      console.log('__getCategories categories', response);
-
-      return response.data;
+      const response = await axios.get(
+        'https://amazing-wallet.herokuapp.com/api/categories?year=2022&month=04',
+      );
+      console.log(
+        '__getCategories categories',
+        response.data.payload.categories[0].category,
+      );
+      return response.data.payload.categories[0].category;
     } catch (error) {
       console.log('__getCategories error.message', error.message);
       return rejectWithValue(error.message);
