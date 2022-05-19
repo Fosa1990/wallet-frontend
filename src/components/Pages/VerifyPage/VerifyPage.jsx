cc22a3a93f54d9f5b8566b7f87098861a2cef90f
Fosa1990 <fosa1990@gmail.com>
2022-05-19 05:46:29 +0300

    added verify page
    TESTING

---
+++ src/components/Pages/VerifyPage/VerifyPage.jsx
@@ -0,0 +1,9 @@
+export default function VerifyPage() {
+  return (
+    <>
+      <p>Greetings</p>
+      <p>[USERNAME] you verified your [EMAIL] successful</p>
+      <a href="http://localhost:3000/">Click to Login</a>
+    </>
+  );
+}
