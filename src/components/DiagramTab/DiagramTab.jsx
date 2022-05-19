f541833327b1034837d01c5acf65593128627e9d
MariaShebeko <marygro89@gmail.com>
2022-05-19 11:45:26 +0200

    added render categories on chart without selectors

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,49 +1,35 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Table from '../Table';
 import Chart from '../Chart';
 import Select from '../Select';
 import styled from 'styled-components';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import categoriesSelectors from '../../redux/categories/categoriesSelectors'
-import {getCategories} from '../../redux/categories/categoriesOperations'
-import transactions from '../Table/transactions.json';
+import categoriesSelectors from '../../redux/categories/categoriesSelectors';
+import { getCategories } from '../../redux/categories/categoriesOperations';
 
 // баланс в диаграмму подставить когда будет готов компонент баланс
 export default function DiagramTab() {
-  // const { getState } = useStore();
-  // const { auth } = getState();
-  // const token = auth.token;
-  const categories = useSelector(categoriesSelectors.getCategories)
-  console.log('__categories DiagramTab', categories);
+  const categories = useSelector(categoriesSelectors.getCategories);
+  // const month = useSelector(categoriesSelectors.getMonth);
+  // const year = useSelector(categoriesSelectors.getYear);
 
+  console.log('__categories DiagramTab', categories);
   const dispatch = useDispatch();
 
-    useEffect(() => {
-    console.log('USEEFFECT');
-    dispatch(getCategories())
-  }, [dispatch])
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
   // const [searchParams, setSearchParams] = useSearchParams();
   // console.log('searchParams', searchParams);
-
-  // Данные для рендера диаграмы из json
-  // const categories = transactions.map(transaction => transaction.category);
-  const colors = transactions.map(transaction => transaction.color);
-  const sums = transactions.map(transaction => transaction.sum);
-
-  useEffect(() => {
-    dispatch(getCategories);
-  }, [dispatch]);
 
   return (
     <>
       <ChartWrapper>
-        <Balance>
-          &#8372;&nbsp; {transactions.length > 0 ? 'balance' : 0}
-        </Balance>
-        <Chart categories={categories} colors={colors} sums={sums} />
+        <Balance>&#8372;&nbsp; {categories.length > 0 ? 'balance' : 0}</Balance>
+        <Chart categories={categories} />
       </ChartWrapper>
       <Select />
-      <Table transactions={transactions} />
+      <Table categories={categories} />
     </>
   );
 }
