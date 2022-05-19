3b186c9ede7d49e41b905a2128baaf5bd126abea
MariaShebeko <marygro89@gmail.com>
2022-05-19 11:46:36 +0200

    added getMonth and getYear actions

--- src/redux/categories/categoriesActions.js
+++ src/redux/categories/categoriesActions.js
@@ -1,3 +1,5 @@
 import { createAction } from '@reduxjs/toolkit';
 
 export const getCategories = createAction('categories/getCategories');
+export const getMonth = createAction('categories/getMonth');
+export const getYear = createAction('categories/getYear');
