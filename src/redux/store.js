74a94b99fcf0bc47018e20b090308a8fe2d86fcc
Fosa1990 <fosa1990@gmail.com>
2022-05-20 06:34:03 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -28,7 +28,11 @@ const authPersistConfig = {
 
 export const store = configureStore({
   reducer: {
-    auth: persistReducer(authPersistConfig, authSlice.reducer),
+    auth: persistReducer(
+      authPersistConfig,
+      authSlice.reducer,
+      // googleSliceReducer,
+    ),
     global: globalReducer,
     [authApi.reducerPath]: authApi.reducer,
     [transactionApi.reducerPath]: transactionApi.reducer,
