0775f14a913547f4e0c4dfde12368e8cf02135af
Maryna Ostapliuk <81308158+marynaost@users.noreply.github.com>
2022-05-20 11:57:01 +0300

    fix

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -6,15 +6,16 @@ import { size, accentBgCl } from '../../stylesheet/utils/stylesVars';
 export default function Avatar() {
   const userName = useSelector(authSelectors.getUserName);
   const avatar = useSelector(authSelectors.getAvatarURL);
+  console.log(avatar);
   const letter = userName.split('')[0].toUpperCase();
   const gravatar = avatar.split('.')[1];
 
   return (
     <>
-      {avatar === gravatar ? (
-        <AvatarImg src={avatar} alt="Avatar" />
-      ) : (
+      {avatar.includes(gravatar) ? (
         <UserAvatarDiv> {letter}</UserAvatarDiv>
+      ) : (
+        <AvatarImg src={avatar} alt="Avatar" />
       )}
     </>
   );
