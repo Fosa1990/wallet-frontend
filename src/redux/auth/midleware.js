a49f835e03ec226c71bde6761529a31b8ad9da11
Maryna Ostapliuk <81308158+marynaost@users.noreply.github.com>
2022-05-23 01:08:04 +0300

    fix token

--- src/redux/auth/midleware.js
+++ src/redux/auth/midleware.js
@@ -3,12 +3,14 @@ import { toast } from 'react-toastify';
 export const rtkQueryErrorLogger = api => next => action => {
   if (isRejectedWithValue(action)) {
     if (action.payload?.data?.payload?.message === 'jwt expired') {
-      toast.error('Please login again');
+      toast.error('Please login again', { className: 'Toastify__error' });
       window.localStorage.clear();
     } else if (action.payload?.data?.payload?.message) {
-      toast.error(`${action.payload?.data?.payload?.message}`);
+      toast.error(`${action.payload?.data?.payload?.message}`, {
+        className: 'Toastify__error',
+      });
     } else {
-      toast.error('Bad Request');
+      toast.error('Bad Request', { className: 'Toastify__error' });
     }
   }
 
